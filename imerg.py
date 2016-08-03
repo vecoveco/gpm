ddc46ff677b8fe2c960a60d77891105434e2541a
Velibor Pejcic <velibor.pejcic@gmx.de>
2016-08-03 14:14:53 +0200

    EDT:  plt global gporf imerg

--- imerg.py
+++ imerg.py
@@ -28,12 +28,17 @@ def plot_borders(ax):
 ## Einlesen von IMERG GPM global
 ## -----------------------------
 
-pfad = ('/user/velibor/SHKGPM/data/imerg/*.HDF5')
+pfad = ('/user/velibor/SHKGPM/data/imerg/3B*.HDF5')
 pfad_imerg = sorted(glob.glob(pfad))
 pfad_imerg = pfad_imerg[0]
 
+pfadg = ('/user/velibor/SHKGPM/data/imerg/2A*.HDF5')
+pfadg_imerg = sorted(glob.glob(pfadg))
+pfadg_imerg = pfadg_imerg[0]
+
 gpmi = h5py.File(pfad_imerg,'r')
-print ('IMERGE Variablen:', gpmi[u'Grid'].keys())
+gpmp = h5py.File(pfadg_imerg,'r')
+
 
 gpmi_lat = gpmi[u'Grid'][u'lat']
 gpmi_lon = gpmi[u'Grid'][u'lon']
@@ -42,6 +47,16 @@ gpmi_lon = gpmi[u'Grid'][u'lon']
 #gpmi_pre = gpmi[u'Grid'][u'probabilityLiquidPrecipitation']
 gpmi_pre = gpmi[u'Grid'][u'IRprecipitation']
 
+gpmp_lat = gpmp[u'S1'][u'Latitude']
+gpmp_lon = gpmp[u'S1'][u'Longitude']
+#gpmi_pre = gpmi[u'Grid'][u'HQprecipitation']
+#gpmi_pre = gpmi[u'Grid'][u'precipitationUncal']
+#gpmi_pre = gpmi[u'Grid'][u'probabilityLiquidPrecipitation']
+gpmp_pre = gpmp[u'S1'][u'surfacePrecipitation']
+
+gpmp_pre = np.array(gpmp_pre)#.transpose()
+gpmp_pre[gpmp_pre==-9999.9] = np.nan
+gpmp_pre = np.ma.masked_invalid(gpmp_pre)
 
 gpmi_pre = np.array(gpmi_pre).transpose()
 gpmi_pre[gpmi_pre==-9999.9] = np.nan
@@ -49,27 +64,31 @@ gpmi_pre = np.ma.masked_invalid(gpmi_pre)
 
 
 
-print ('IMERG Rain Shape:',gpmi_pre.shape)
-print ('IMERG Lat Shape:', gpmi_lat.shape)
-print ('IMERG Lon Shape:', gpmi_lon.shape)
-print ('IMERG Rain Range:',np.nanmin(gpmi_pre),' bis ',np.nanmax(gpmi_pre))
-
 ## Plot
 ## ----
 
-gpmvmax = np.nanmax(gpmi_pre)
-
 fig = plt.figure()
-ax = fig.add_subplot(111, aspect='equal')
-pm2 = plt.pcolormesh(gpmi_lon, gpmi_lat, gpmi_pre,vmin=0,vmax=10)#,vmin=0,vmax=maxv)
+ax = fig.add_subplot(211, aspect='equal')
+pm2 = plt.pcolormesh(gpmi_lon, gpmi_lat, np.ma.masked_invalid(gpmi_pre),vmin=0,vmax=10)#,vmin=0,vmax=maxv)
 cbar = plt.colorbar(pm2, shrink=0.75)
 plot_borders(ax)
 plt.xlabel("lon")
 plt.ylabel("lat")
 plt.xlim(-180,180)
 plt.ylim(-90,90)
+plt.title(str(pfad_imerg[-60:-1]))
 plt.grid(True)
 
-
+ax = fig.add_subplot(212, aspect='equal')
+pm2 = plt.pcolormesh(gpmp_lon, gpmp_lat, np.ma.masked_invalid(gpmp_pre),vmin=0,vmax=10)#,vmin=0,vmax=maxv)
+cbar = plt.colorbar(pm2, shrink=0.75)
+plot_borders(ax)
+plt.xlabel("lon")
+plt.ylabel("lat")
+plt.xlim(-180,180)
+plt.ylim(-90,90)
+plt.title(str(pfadg_imerg[-60:-1]))
+plt.grid(True)
 plt.show()
+
 
